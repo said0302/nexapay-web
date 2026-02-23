1ee44e10712333ef3cc667ffe9c2af5aa1e92860
said0302 <saiduljalali@gmail.com>
2026-02-23 09:03:00 +0800

    Misi Terakhir: Menambahkan fitur PWA (Install Aplikasi)

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true, // Supaya bisa dites di localhost
+      },
+      manifest: {
+        name: "NexaPay - AI Expense Tracker",
+        short_name: "NexaPay",
+        description: "Aplikasi pencatat keuangan pintar berbasis AI",
+        theme_color: "#ffffff",
+        background_color: "#F2F2F7",
+        display: "standalone", // Ini yang bikin layarnya full screen!
+        icons: [
+          {
+            src: "/logo-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/logo-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
